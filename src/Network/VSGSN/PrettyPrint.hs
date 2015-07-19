aee1523def30f22f5794a9ba98a795d896138268
k32 <skmshj@gmail.com>
2015-07-19 17:43:54 +0300

    Finished header formatting

--- src/Network/VSGSN/PrettyPrint.hs
+++ src/Network/VSGSN/PrettyPrint.hs
@@ -1,10 +1,13 @@
-{-# LANGUAGE UnicodeSyntax, OverloadedStrings, LambdaCase, FlexibleContexts #-}
+{-# LANGUAGE UnicodeSyntax, OverloadedStrings, LambdaCase, FlexibleContexts,
+             NoMonomorphismRestriction #-}
 module Network.VSGSN.PrettyPrint (
     defaultHeaderFormat
   , defaultLineFormat
   , makeLogEntryPP
   ) where
-
+    
+import Data.String
+import Data.Fixed
 import Pipes
 import Pipes.ByteString as P
 import Text.Format
@@ -15,8 +18,8 @@ import Network.VSGSN.Types
 import Network.VSGSN.Logs.Types
 
 defaultHeaderFormat, defaultLineFormat ∷ B.ByteString
-defaultHeaderFormat = "~~~ ${YY}-${MM}-${DD} ${hh}:${mm}:${ss} (${origin}) ~~~\n"
-defaultLineFormat = "$mm:$ss> $l"
+defaultHeaderFormat = "\n~~~ ${YY}-${MM}-${DD} ${hh}:${mm}:${ss} (${origin}) ~~~\n"
+defaultLineFormat = "\n$mm:$ss> "
 
 makeLogEntryPP ∷ (Monad m2, MonadWarning [String] e m) 
                ⇒ B.ByteString
@@ -42,14 +45,19 @@ printLogEntry hf lf = forever $ do
     , _basic_text = txt
     } ← await
   let (year, month, day) = toGregorian date
+      TimeOfDay hour minute sec = timeToTimeOfDay time
       format' a b = P.fromLazy . toLazyByteString $ format a b -- TODO: Is it effective?
-      p0 x | x<10 = char8 '0' <> int8Dec x
-           | True = int8Dec x
+      p0 s x | x<10 = char8 '0' <> s x
+             | True = s x
+      i8f = int8Dec . fromIntegral
       f = \case
             "YY" → int32Dec $ fromIntegral year
-            "MM" → p0 $ fromIntegral month
-            "DD" → p0 $ fromIntegral day
-            --"hh" → p0 . fromIntegral $ 
+            "MM" → p0 i8f month
+            "DD" → p0 i8f day
+            "hh" → p0 i8f hour
+            "mm" → p0 i8f minute
+            "ss" → p0 (fromString . showFixed True) sec
+            "origin" → byteString . fromString $ show origin
             a → byteString a
       lb = format lf f
       --txt' = {- B.intercalate lb $-} B.split '\n' txt
