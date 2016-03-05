a41de9e3a5193cc4e5b7aaa8b4a46a981ebaf054
k32 <example@example.com>
2016-03-05 12:56:51 +0100

    Added colors to the Emacs mode

--- emacs/logmerger-mode.el
+++ emacs/logmerger-mode.el
@@ -12,6 +12,9 @@
 (defconst logmerger-re-any-origin
   ".*")
 
+(defconst logmerger-re-line-begin
+  "|.*|")
+
 (cl-defstruct logmerger-entry timestamp origin)
 
 (cl-defstruct logmerger-origin (boring nil) face)
@@ -62,9 +65,95 @@
                                                     :face nil))))
       (make-logmerger-entry :timestamp date :origin origin))))
 
-(defvar logmerger-highlights
-  '((logmerger-re-begin-entry . font-lock-string-face)
-    (logmerger-re-line-begin . font-lock-comment-face)))
+;; (defvar logmerger-highlights
+;;    (list (list logmerger-re-begin-entry '(1 font-lock-string-face))
+;;          (list logmerger-re-line-begin '(1 font-lock-comment-face))))
+
+;(defcustom logmerger-severity-levels 
+
+
+(defface logmerger-error-face
+  '((((class color) (min-colors 8))
+     :background "red" :foreground "white")
+    (t :inverse-video t))
+  "Errors in the logs"
+  :group 'logmerger)
+
+(defvar logmerger-error-face 'logmerger-error-face
+  "Face to display errors")
+
+(defvar logmerger-line-begin-face 'font-lock-comment-face
+  "Face to display ine beginnings")
+
+(defface logmerger-severe-accident-face
+  '((((class color) (min-colors 8))
+     :background "yellow" :foreground "red")
+    (t :inverse-video t))
+  "Severe incident"
+  :group 'logmerger)
+
+(defvar logmerger-severe-accident-face 'logmerger-severe-accident-face
+  "Face to display severe accidents")
+
+(defface logmerger-user-intervention-face
+  '((((class color) (min-colors 8))
+     :foreground "green")
+    (t :inverse-video t))
+  "User intervention to the node"
+  :group 'logmerger)
+
+(defvar logmerger-user-intervention-face 'logmerger-user-intervention-face
+  "Face to display user intervention")
+
+(defface logmerger-entry-header-face
+  '((((class color) (min-colors 8))
+     :background "gray" :foreground "black")
+    (t :inverse-video t))
+  "Beginning of an entry"
+  :group 'logmerger)
+
+(defvar logmerger-entry-header-face 'logmerger-entry-header-face
+  "Entry header")
+
+(defcustom logmerger-user-intervention
+  '("gsh:<[0-9]+.[0-9]+.[0-9]+>,.*"
+    "\\(REPLY\\|REQUEST\\) message-id=\".*")
+  "User intervention"
+  :type '(repeat string)
+  :group 'logmerger)
+
+(defcustom logmerger-severe-accidents
+  '("node_restart.*"
+    "restart_node.*"
+    "restart_dpe.*"
+    "isp\\.log"
+    )
+  "Severe incidents"
+  :type '(repeat string)
+  :group 'logmerger)
+  
+(defcustom logmerger-errors
+  '(".*\\(ERROR\\|CRASH\\) +\\(REPORT\\)?.*"
+    "error"
+    ".*capsule_failure.*")
+  "Regex matching severe errors"
+  :type '(repeat string)
+  :group 'logmerger)
+  
+(defun logmerger-merge-regexes (l)
+  (reduce 
+   (lambda (a b) (concat a "\\|" b))
+   (mapcar 
+    (lambda (a) (concat "\\(" a "\\)"))
+    l)))
+
+(setq logmerger-highlights
+      `((,logmerger-re-line-begin . logmerger-line-begin-face)
+        (,(concat logmerger-re-begin-entry ".*") . logmerger-entry-header-face)
+        (,(logmerger-merge-regexes logmerger-errors) . logmerger-error-face)
+        (,(logmerger-merge-regexes logmerger-severe-accidents) . logmerger-severe-accident-face)
+        (,(logmerger-merge-regexes logmerger-user-intervention) . logmerger-user-intervention-face)
+        ))
 
 ;;;; Moving around ;;;;
 
@@ -117,8 +206,10 @@
 (define-derived-mode logmerger-mode text-mode "Log"
   "Major mode for viewing LogMerger output"
   (read-only-mode t)
-  (setq font-lock-defaults '(() nil nil nil logmerger-highlights))
-  (setq logmerger-origins '()))
+  (setq-local font-lock-defaults 
+              '(logmerger-highlights))
+  (setq-local logmerger-origins '())
+  (make-local-variable 'logmerger-symbols))
 
 (define-key logmerger-mode-map (kbd "b") 'logmerger-toggle-boring-origin)
 
@@ -141,6 +232,21 @@
 (logmerger-defgo "-origin" " to an entry with the same origin" "["
                  backward 'logmerger-same-origin)
 
+(defun logmerger-go-next-error (n)
+  (interactive "p")
+  (dotimes (i n) 
+    (re-search-forward (logmerger-merge-regexes logmerger-errors))))
+
+
+(define-key logmerger-mode-map (kbd "e") 'logmerger-go-next-error)
+
+(defun logmerger-go-prev-error (n)
+  (interactive "p")
+  (dotimes (i n) 
+    (re-search-backward (logmerger-merge-regexes logmerger-errors))))
+
+(define-key logmerger-mode-map (kbd "E") 'logmerger-go-prev-error)
+
 ;; Time
 ;; (defmacro logmerger-go-dt (direction dt)
 ;;   (let ((ta (make-symbol "ta"))
@@ -155,4 +261,9 @@
 
 (logmerger-defgo "-minute" "" "m" forward (logmerger-go-dt forward 60))
 
+(defun logmerger-add-symbol-to-overview ()
+  (interactive)
+  ())
+
 (provide 'logmerger-mode)
+
